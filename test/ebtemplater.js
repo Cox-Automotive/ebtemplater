ffdcbe8ee718deb279f52177c60002c59bf693d7
Brian <brian.antonelli@gmail.com>
2016-11-07 14:09:42 -0500

    tess

--- test/ebtemplater.js
+++ test/ebtemplater.js
@@ -1,8 +1,8 @@
 /*jslint node: true */
 'use strict';
 
-var ebtemplater = require('../bin/ebtemplater');
-var assert   = require('chai').assert;
+// var ebtemplater = require('../bin/ebtemplater');
+var assert      = require('chai').assert;
 
 describe('ebtemplater', function(){
     it('should write some tests', function(done){
